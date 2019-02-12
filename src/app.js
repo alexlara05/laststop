7caca8e290b9b4acdb528723f6863a846214b8f4
Hector Lara <djnegrosabio@gmail.com>
2019-02-11 21:15:34 -0500

    b

--- src/app.js
+++ src/app.js
@@ -16,11 +16,11 @@ const nexmo = new Nexmo({
 });
 
 // Settings
-app.set('port', process.env.PORT || 5000);
+app.set('port', process.env.PORT || 3000);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(session({ secret: '12dfdfdfsd.aj.l&&3456', resave: true, saveUninitialized: true }));
-process.env.NODE_ENV = 'production';
+
 // register the bodyParser middleware for processing forms
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -834,7 +834,7 @@ app.post("/send_sms", function (req, res, next) {
 
 // Static Files
 app.use('/public', express.static(__dirname + '/public'));
-app.listen(5000, () => {
+app.listen(3000, () => {
     console.log('running LastStop server');
 });
 
